488d1c2a75096bfd9bae96ee2e4f85ee44b10d0d
Front-End <Front-End>
2016-02-21 19:13:56 +0200

    gulp install

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp'),
+	concatCss = require('gulp-concat-css'),
+	cssmin = require('gulp-cssmin'),
+	rename = require('gulp-rename');
+	watch = require('gulp-watch'),
+
+gulp.task('css', function () {
+  return gulp.src('./public/css/*.css')
+    .pipe(concatCss("bundle.css"))
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch("./public/css/*.css", ['css']);
+    
+});
