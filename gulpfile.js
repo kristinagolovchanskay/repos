beb4e2d4e4a1706157cac33d72ea470ce09f34fe
kristinagolovchanskay <2005_kristina@mail.ru>
2016-02-22 23:01:01 +0200

    add new gulp modules

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ var gulp = require('gulp'),
 	cssmin = require('gulp-cssmin'),
 	rename = require('gulp-rename');
 	watch = require('gulp-watch'),
+	stylus = require('gulp-stylus'),
+	uglify = require('gulp-uglify');
+	imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
 
 gulp.task('css', function () {
   return gulp.src('./public/css/*.css')
@@ -17,11 +21,34 @@ gulp.task('watch', function() {
     
 });
 
-var gulp = require('gulp'),
-	concatCss = require('gulp-concat-css');
-
 gulp.task('concat', function () {
   return gulp.src('./public/css/*.css')
     .pipe(concatCss("bundle.css"))
     .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('styl', function() {
+    return gulp.src('./public/styl/*.styl')
+        .pipe(stylus({
+            linenos: false
+        }))
+        .pipe(concatCss('styl.css'))
+        .pipe(gulp.dest('./public/css/'));
+
+});
+
+gulp.task('js', function () {
+    return gulp.src('./public/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./public/build/'));
+});
+
+gulp.task('image', function () {
+    return gulp.src('public/images/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('public/images/'));
 });
